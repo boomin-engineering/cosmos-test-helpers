5f52b409dfd2530df3e76a6d4d69e70c3859e538
Andrew Butler <butlerat@gmail.com>
2022-07-27 09:36:49 +0100

    Initial version (#1)
    * Initial version ported from platform code (still needs tidying up)

---
+++ CosmosTestHelpers.IntegrationTests/TestModels/TestEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmosTestHelpers.IntegrationTests.TestModels
+{
+    public enum TestEnum
+    {
+        Value1,
+
+        Value2
+    }
+}
---
+++ CosmosTestHelpers/InvocationInformation.cs
@@ -0,0 +1,12 @@
+﻿namespace CosmosTestHelpers
+{
+    public class InvocationInformation
+    {
+        public string MethodName { get; }
+
+        public InvocationInformation(string methodName)
+        {
+            MethodName = methodName;
+        }
+    }
+}
